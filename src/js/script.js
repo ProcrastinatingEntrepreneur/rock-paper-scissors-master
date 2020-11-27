4adcb7165697705b38ed574758635c45f7c27992
ProcrastinatingEntrepreneur <benjamin.metcalfe@my.tccd.edu>
2020-11-26 19:56:57 -0600

    Finished core functionality, need to add rules still.

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,8 @@ let score = 0;
 function gameChoice(choice) {
     if (choice === 'scissors') {
         console.log('You chose scissors');
-        let computerChoiceIndex = Math.floor(Math.random() * 3);
-        if (computerChoiceIndex === 0) {
+        let computerChoiceIndex = Math.floor(Math.random() * 4);
+        if (computerChoiceIndex === 1) {
             console.log('Computer won!');
             if (score > 0) {
                 score--;
@@ -24,17 +24,62 @@ function gameChoice(choice) {
             console.log('You win!');
             console.log(score);
         }
-    } else {
+    } 
+
+    if (choice === 'paper') {
+        console.log('You chose paper');
+        let computerChoiceIndex = Math.floor(Math.random() * 4);
+        if (computerChoiceIndex === 2) {
+            console.log('Computer won!');
+            if (score > 0) {
+                score--;
+            } else {
+                score = 0;
+            }
+                console.log(score);
+            } else {
+                if (score >= 0) {
+                    score++
+            }
+            document.querySelector('.score-count').innerHTML = score;
+            console.log('You win!');
+            console.log(score);
+        }
+    } 
+
+    if (choice === 'rock') {
+        console.log('You chose rock');
+        let computerChoiceIndex = Math.floor(Math.random() * 4);
+        if (computerChoiceIndex === 3) {
+            console.log('Computer Won');
+            if (score > 0) {
+                score--;
+            } else {
+                score = 0;
+            }
+            console.log(score)
+            if (score >= 0) {
+                    score++
+            }
+            document.querySelector('score-count').innerHTML = score;
+            console.log('You win');
+            console.log(score);
+            }
+        }
+    }
+    /*
         if (choice === 'paper') {
             computerChoiceIndex = Math.floor(Math.random() * 3);
             if (computerChoiceIndex === 2) {
                 console.log('Computer won!');
                 if (score > 0) {
                     score--;
-                } else {
-                    
                 }
+                if (score >= 0) {
+                    score++;
+                } 
+                    document.querySelector('.score-count').innerHTML = score++;
             }
         }
-    }
-}
+    */
+
